c418d5819bd33bba4eab75f76597d3b24cead650
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2024-04-21 19:29:24 -0300

    feat: api && api docs

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# inteli-modulo-10-ponderada-1
+# Inteli módulo 10 ponderada 1
+
+_Instruções_
+
+```
+Pessoal nossa atividade ponderada vai ser desenvolvida em diversas frentes e por partes. A primeira parte vai ser a construção da API. Existe aqui um único pré-requisito, ela precisa ser SINCRONA!!!. Mais sobre esse ponto no nosso próximo encontro.
+
+O que vocês devem entregar, em um repositório do Github (até as 23h59 de hoje, dia 21/04/2024 - pessoal nesse ponto eu não vou ser flexível na entrega, pois o objetivo é que vocês consigam desenvolver em sala e entrega extendida é só para contemplar quem não conseguiu terminar em sala):
+
+Collections do Insomnia para testar a API
+YAML do OpenAPI (Swagger) para documentar a API
+Código fonte da API
+Instruções para executar a API
+Obrigatóriamente ela deve ser uma API de grau de maturidade 2, ou seja, ela deve ser capaz de fazer a autenticação do usuário e permitir que ele crie, leia, atualize e delete tarefas. Você podem utilizar o código fornecido como base.
+
+```
+
+## API de Gerenciamento de Tarefas
+
+Esta é uma API Flask desenvolvida para gerenciar tarefas com autenticação de usuários e controle de uma to-do list.
+
+> [!IMPORTANT]
+> A to-do list está sendo administrada em memória! Ou seja, ao reiniciar a aplicação, todos os dados serão perdidos.
+
+## Instalação
+
+> [!NOTE]
+> Antes de iniciar, você precisará ter o Python instalado em sua máquina.
+
+Para rodar esta aplicação, você precisa instalar as dependências listadas no arquivo `requirements.txt`:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Executando a API
+
+Para iniciar a API, rode o seguinte comando na root desse repositório:
+
+```bash
+python main.py
+```
+
+Agora você pode acessar a API em [http://localhost:3001](http://localhost:3001).
+
+## Documentação da API
+
+Após iniciar a API, você pode acessar a documentação da API via Swagger UI pelo seguinte URL: [http://localhost:3001/docs](http://localhost:3001/docs).
+
+Acesse este link para interagir com a API através da interface Swagger, onde você pode testar os endpoints diretamente.
+
+Você pode encontrar a documentação da API em OpenAPI (Swagger) no arquivo [swagger.yaml](./static/swagger.yml).
+
+## Endpoints
+
+A API consiste nos seguintes endpoints:
+
+- `POST /create_user`: Cria um novo usuário (acesso restrito a administradores).
+- `POST /create_task`: Permite a um usuário autenticado criar uma nova tarefa.
+- `GET /tasks`: Retorna todas as tarefas do usuário autenticado.
+- `PUT /update_task`: Atualiza o status de uma tarefa específica.
+- `DELETE /delete_task`: Remove uma tarefa específica.
+
+Cada endpoint requer que o usuário esteja autenticado, e alguns endpoints exigem que o usuário seja um administrador (nesse caso, apenas `/create_user`).
---
+++ main.py
@@ -0,0 +1,218 @@
+from functools import wraps
+
+from flask import Flask, g, jsonify, request
+from flask_swagger_ui import get_swaggerui_blueprint
+
+app = Flask(__name__)
+
+# Configuração do Swagger UI
+SWAGGER_URL = "/docs"  # URL para acessar a UI do Swagger - ajuste conforme necessário
+API_URL = "/static/swagger.yml"  # Caminho para o arquivo das especificações da API
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={"app_name": "To-do API"},  # Configurações adicionais opcionais
+)
+
+app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+admins: dict[str, bool] = {
+    "Murilo": True,
+    "Nicola": True,
+    "Muricola": True,
+    "Andre": True,
+}
+
+tasks: dict[str, list[dict[str, str]]] = {}
+
+
+def auth(f):
+    @wraps(f)
+    def auth_function(*args, **kwargs):
+        try:
+            user = request.headers.get("Authorization")
+
+            if user is None:
+                raise KeyError("No auth provided")
+
+            verify = tasks.get(user)
+
+            if verify is None or verify == False:
+                raise KeyError("User not authorized")
+
+            g.user = user
+            return f(*args, **kwargs)
+
+        except Exception as err:
+            return jsonify({"message": f"{err}"}), 401
+
+    return auth_function
+
+
+def admin_auth(f):
+    @wraps(f)
+    def admin_auth_function(*args, **kwargs):
+        try:
+            user = request.headers.get("Authorization")
+
+            if user is None:
+                raise KeyError("No auth provided")
+
+            verify = admins.get(user)
+
+            if verify is None or verify == False:
+                raise KeyError("User not authorized")
+
+            g.user = user
+            return f(*args, **kwargs)
+
+        except Exception as err:
+            return jsonify({"message": f"{err}"}), 401
+
+    return admin_auth_function
+
+
+@app.post("/create_user")
+@admin_auth
+def create_user_route():
+    if request.is_json:
+        data = request.get_json()
+
+        user_name = data.get("user_name")
+
+        if user_name is None or type(user_name) != str:
+            return jsonify(message="No user_name provided"), 400
+
+        tasks[user_name] = []
+
+        return jsonify(message="User created with success!"), 201
+    else:
+        return jsonify(message="The request body must be a json"), 400
+
+
+@app.post("/create_task")
+@auth
+def create_task_route():
+    if request.is_json:
+        user_name = getattr(g, "user", None)
+
+        if user_name is None:
+            return jsonify(message="Auth didn't provided user"), 404
+
+        data = request.get_json()
+
+        to_do_task = data.get("task")
+
+        if to_do_task is None or type(to_do_task) != str:
+            return jsonify(message="No user_name provided"), 400
+
+        this_user_tasks = tasks.get(user_name)
+
+        if this_user_tasks is None:
+            return jsonify(message="Create user firts"), 500
+
+        task_to_add = {
+            "id": len(this_user_tasks) + 1,
+            "task": to_do_task,
+            "status": "pending",
+        }
+
+        tasks[user_name].append(task_to_add)
+
+        return jsonify(message="Task created with success"), 201
+
+    else:
+        return jsonify(message="The request body must be a json"), 400
+
+
+@app.get("/tasks")
+@auth
+def get_all_tasks_routes():
+    user_name = getattr(g, "user", None)
+
+    if user_name is None:
+        return jsonify(message="Auth didn't provided user"), 404
+
+    this_tasks = tasks.get(user_name)
+
+    if this_tasks is None:
+        return jsonify(message="This user doesn't have any task"), 500
+
+    return jsonify(tasks=this_tasks), 200
+
+
+@app.put("/update_task")
+@auth
+def update_task_route():
+    if request.is_json:
+        user_name = getattr(g, "user", None)
+
+        if user_name is None:
+            return jsonify(message="Auth didn't provided user"), 404
+
+        data = request.get_json()
+
+        task_id = data.get("task_id")
+        new_task_status = data.get("status")
+        modified = False
+
+        if new_task_status is None or type(new_task_status) != str or task_id is None:
+            return jsonify(message="Uncomplited body"), 400
+
+        this_user_tasks = tasks.get(user_name)
+
+        if this_user_tasks is None:
+            return jsonify(message="Create user firts"), 500
+
+        for task in this_user_tasks:
+            if task["id"] == task_id:
+                task["status"] = new_task_status
+                modified = True
+
+        if modified == True:
+            return jsonify(message="Task modified with success"), 202
+
+        return jsonify(message="Task doesn't exists"), 500
+
+    else:
+        return jsonify(message="The request body must be a json"), 400
+
+
+@app.delete("/delete_task")
+@auth
+def delete_task_route():
+    if request.is_json:
+        user_name = getattr(g, "user", None)
+
+        if user_name is None:
+            return jsonify(message="Auth didn't provided user"), 404
+
+        data = request.get_json()
+
+        task_id = data.get("task_id")
+        modified = False
+
+        if task_id is None:
+            return jsonify(message="Uncomplited body"), 400
+
+        this_user_tasks = tasks.get(user_name)
+
+        if this_user_tasks is None:
+            return jsonify(message="Create user firts"), 500
+
+        for i, task in enumerate(this_user_tasks):
+            if task["id"] == task_id:
+                del this_user_tasks[i]
+                modified = True
+
+        if modified == True:
+            return jsonify(message="Task deleted with success"), 203
+
+        return jsonify(message="Task doesn't exists"), 500
+
+    else:
+        return jsonify(message="The request body must be a json"), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3001, debug=True, threaded=False)
